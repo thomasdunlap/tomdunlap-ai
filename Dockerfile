fca57955438dc951848fc4e9523d855cec28846f
thomasdunlap <tom.a.dunlap@gmail.com>
2020-07-03 11:38:02 -0700

    First commit.

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+from ubuntu:latest
+
+RUN apt-get update -y
+RUN apt-get install -y build-essential python3 python3-pip
+
+COPY . /webapp
+WORKDIR /webapp
+
+RUN pip3 install -r requirements.txt
+
+CMD gunicorn --bind 0.0.0.0:$PORT wsgi
---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+port = int(os.environ.get("PORT", 5000))
+
+@app.route('/')
+def home():
+    return render_template("home.html") 
+
+@app.route('/tomisagod')
+def tom_is_a_god():
+    return 'Tom is the greatest'
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=port)
